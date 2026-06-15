240af9d9155dc68d8ded56f67ace14981c985412
gopinath210805 <higopinathashok@gmail.com>
2026-06-15 22:24:04 +0530

    Add dashboard page

---
+++ datasets/src/app/dashboard/page.js
@@ -0,0 +1,13 @@
+"use client";
+
+export default function DashboardPage() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Learning Dashboard</h1>
+
+      <p>
+        Dashboard is working.
+      </p>
+    </div>
+  );
+}
